6606d983e1564d4edd2045708c9c7c4c67708f58
Mukulj0shi <77253989+Mukulj0shi@users.noreply.github.com>
2022-08-13 01:16:25 +0530

    Add files via upload
    Created snake body with 3 square.

--- create_snakev2.py
+++ create_snakev2.py
@@ -8,7 +8,9 @@
 
 from turtle import Turtle, Screen
 
-turtle_obj = Turtle()
+turtle_obj1 = Turtle()
+turtle_obj2 = Turtle()
+turtle_obj3 = Turtle()
 screen_obj = Screen()
 screen_obj.screensize(canvwidth=600, canvheight=600)
 screen_obj.bgcolor("black")
@@ -16,11 +18,24 @@ screen_obj.title("Snake Game")
 screen_obj.listen()
 is_game_on = True
 
+
+
 #Define snake body
+turtle_obj1.goto(0,0)
+turtle_obj1.shape("square")
+turtle_obj1.fillcolor("white")
+
+
+turtle_obj2.goto(-20,0)
+turtle_obj2.shape("square")
+turtle_obj2.fillcolor("white")
+
+
+turtle_obj3.goto(-40,0)
+turtle_obj3.shape("square")
+turtle_obj3.fillcolor("white")
 
 
 
-
-snake_body()
 
 screen_obj.exitonclick()
