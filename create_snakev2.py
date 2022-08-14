33b6783e800e44be8fe6cdb799417ef4a3c5f9bb
Mukulj0shi <77253989+Mukulj0shi@users.noreply.github.com>
2022-08-14 14:13:30 +0530

    snake body and move
    Created snake class : defines snake body and move
    Created main: Uses snake class to move the snake in different directions.

--- create_snakev2.py
+++ create_snakev2.py
@@ -1,56 +1,58 @@
 #!/usr/bin/env python
 
 """
-# This module include Creating snake using turtle module.
-# Moving snake forward and changing snake direction based on the inputs from keyword.
-# Function snake_body defines the snake body.
-# Function move_snake_fwd responsible for moving the snake and changing its direction.
+# This is snake class.
+# Create snake body by calling the Snake() class
+# Move snake by calling snake_move() method.
 """
 
 from turtle import Turtle, Screen
-import time
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]    #Constant are defined at the top of the program in capital letters and snake case.
+MOVE_DISTANCE = 20
 
-# turtle_obj1 = Turtle()
-screen_obj = Screen()
-screen_obj.tracer(0)
-screen_obj.screensize(canvwidth=600, canvheight=600)
-screen_obj.bgcolor("black")
-screen_obj.title("Snake Game")  # Represent title of the screen.
-screen_obj.listen()
-
-is_game_on = True
-snake_cord = [(0, 0), (-20, 0), (-40, 0), (-60, 0)]
-turt_obj_list = []
-
-# Define snake body
-def create_snake():
-    for cord in snake_cord:
-        turtle_obj1 = Turtle()  # This will create 3 turtle objects.
-        # Specifying object outside loop will create only single object.
-        turtle_obj1.shape("square")
-        turtle_obj1.color("white")
-        turtle_obj1.penup()
-        turtle_obj1.speed(1)
-        turtle_obj1.goto(cord)
-        turt_obj_list.append(turtle_obj1)
-
-def move_snake():
-    while is_game_on:
-        for items in range((len(turt_obj_list)-1), 0, -1):
-            x_cord = turt_obj_list[items - 1].xcor()         #Last item moves to second last item position
-            y_cord = turt_obj_list[items - 2].ycor()
-            turt_obj_list[items].goto(x_cord, y_cord)
-        turt_obj_list[0].forward(20)
-        time.sleep(1)
-        #turt_obj_list[0].left(90)
-        screen_obj.update()
+class Snake():
+    def __init__(self):
+        self.turt_obj_list = []                         # Here we are defining the attributes related to snake creation.
+        self.create_snake()
+        self.head = self.turt_obj_list[0]
 
 
+    # Define snake body
+    def create_snake(self):
+        for cord in STARTING_POSITIONS:
+            turtle_obj1 = Turtle()  # This will create 3 turtle objects.
+            # Specifying object outside loop will create only single object.
+            turtle_obj1.shape("square")
+            turtle_obj1.color("white")
+            turtle_obj1.penup()
+            turtle_obj1.speed(1)
+            turtle_obj1.goto(cord)
+            self.turt_obj_list.append(turtle_obj1)      #Calling attributes in methods using self.attribute_name.
+
+    def move_snake(self):
+        for items in range((len(self.turt_obj_list) - 1), 0, -1):
+            x_cord = self.turt_obj_list[items - 1].xcor()  # Last item moves to second last item position
+            y_cord = self.turt_obj_list[items - 2].ycor()
+            self.turt_obj_list[items].goto(x_cord, y_cord)
+        self.head.forward(MOVE_DISTANCE)               #Calling attributes in methods using self.attribute_name.
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
 
 
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
 
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
 
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
 
-
-
-screen_obj.exitonclick()
+    #create_snake()
+    #move_snake()
+    #screen_obj.exitonclick()
